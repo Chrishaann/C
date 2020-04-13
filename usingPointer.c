c9ab35d10f11709d76462f1b4a7f6a9ae24193ea
Chrishaann <you@example.com>
2020-04-13 10:28:40 +0800

    Added C Language Programs

--- usingPointer.c
+++ usingPointer.c
@@ -13,7 +13,7 @@ int main()
     printf("\n");
     varValue3 = 15;
     pointerValue = &varValue3;
-    printf("AThe pointer value is %d, the pointer address is %p.", *pointerValue, pointerValue);
+    printf("AaThe pointer value is %d, the pointer address is %p.", *pointerValue, pointerValue);
     printf("\n");
     
     
